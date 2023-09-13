61e71542bd4fa3672ad781ff7313984539a662d9
RhettRoseman <rroseman@rcplanners.com>
2023-09-12 23:08:14 -0400

    solved problem

--- assets/script.js
+++ assets/script.js
@@ -2,28 +2,31 @@
 // Criteria for password
 var generateBtn = document.querySelector("#generate");
 // prompts 
-var lowercase = confirm("Password  has lowercase letters ")
-var uppercase = confirm("Password  has uppercase letters")
-var Characters = confirm("Password  has 8-125 characters")
-var special = confirm("pasword has special characters")
+var must = confirm("You must put information into the prompts in order to create password." , "Click OK to confirm")
+var lowercase = prompt("Password  has lowercase letters ")
+var uppercase = prompt("Password  has uppercase letters")
+var Characters = prompt("Password  has 8-125 characters")
+var special = prompt("pasword has special characters")
 // Character banks for password
-// var letters1 = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
-// var letters2 = ("a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-// var numbers = ("1", "2", "3", "4", "5", "6", "7", "8", "9")
-// var special = ("!", "@", "#", "$", "%", "^", "&", "*", "-", "+")
-
+var upper = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+var lower = ( "a", "b", "c", "d", "e", "f", "g", "h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+var num = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+var symbol = ("!", "@", "#", "$", "%", "^", "&", "*", "-", "+")
 // fucnction that makes password usable 
 function generatePassword() {
 console.log( "Use button to generate password")
-// distributes random number and letter generator into box for user to copy and paste 
-// return Math.random(23).toString(36).slice(2);
-return Math.random().toString(36).slice(2);
+return lowercase + uppercase + Characters + special 
 }
+// + upper + lower + num + symbol 
 
 
 
 
-confirm("If satisfied with your password copy and paste. If dissatisfied press Generate Password button again. ");
+
+
+
+
+// prompt("If satisfied with your password copy and paste. If dissatisfied press Generate Password button again. ");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
